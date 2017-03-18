a6a20126f87efb4a06eaf343619f4281e9f7f130
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2017-03-18 13:30:57 +0000

    Added bombsCounter

--- Engine/MineField.cpp
+++ Engine/MineField.cpp
@@ -3,6 +3,7 @@
 #include "Vei2.h"
 #include <assert.h>
 #include <random>
+#include <algorithm>
 
 void MineField::Tile::SpawnMine()
 {
@@ -75,7 +76,13 @@ bool MineField::Tile::IsFlagged() const
 	return (state == State::Flagged);
 }
 
+void MineField::Tile::SetNeighborBombCount(int bombCount)
+{
+	// Only if this tile is initialized
+	assert(nNeighborBombs == -1);
 
+	nNeighborBombs = bombCount;
+}
 
 
 
@@ -104,17 +111,12 @@ MineField::MineField(int nMines)
 		TileAt(spawnPos).SpawnMine();
 	}
 
-
-	// Test to reveal some tiles
-	for (int i = 0; i < 120; i++)
+	for (Vei2 gridPos = { 0,0 }; gridPos.y < height; gridPos.y++)
 	{
-		const Vei2 gridPos = { xDist(rng), yDist(rng) };
-
-		if (!TileAt(gridPos).IsRevealed())
+		for (gridPos.x = 0; gridPos.x < width; gridPos.x++)
 		{
-			TileAt(gridPos).Reveal();
+			TileAt(gridPos).SetNeighborBombCount(CountNeighborBombs(gridPos));
 		}
-		
 	}
 }
 
@@ -175,4 +177,28 @@ Vei2 MineField::ScreenToGrid(const Vei2 & screenPos)
 {
 	// Convert screen position (pixels) into grid position (tiles)
 	return (screenPos/SpriteCodex::tileSize);
+}
+
+int MineField::CountNeighborBombs(const Vei2& gridPos)
+{
+	// Set the boundaries for a gridPos. Maximun 9 tiles covering that gridPos
+	// Taking into account the boundaries of the grid
+	const int xStart = std::max(0, gridPos.x - 1);
+	const int yStart = std::max(0, gridPos.y - 1);
+	const int xEnd = std::min(width - 1, gridPos.x + 1);
+	const int yEnd = std::min(height - 1, gridPos.y + 1);
+
+	int count = 0;
+	for (Vei2 gridPos = { xStart,yStart }; gridPos.y <= yEnd; gridPos.y++)
+	{
+		for (gridPos.x = xStart; gridPos.x <= xEnd; gridPos.x++)
+		{
+			if (TileAt(gridPos).HasMine())
+			{
+				count++;
+			}
+		}
+	}
+
+	return count;
 }
--- Engine/MineField.h
+++ Engine/MineField.h
@@ -24,11 +24,12 @@ private:
 		bool IsRevealed() const;
 		void ToggleFlag();
 		bool IsFlagged() const;
+		void SetNeighborBombCount(int bombCount);
 
 	private:
 		State state = State::Hidden;
-		bool hasBomb = false;
-
+		bool hasBomb = false;		
+		int nNeighborBombs = -1;
 	};
 public:
 	MineField(int nMines);
@@ -36,11 +37,13 @@ public:
 	RectI GetRect() const;
 	void OnRevealClick(const Vei2 screenPos);
 	void OnFlagClick(const Vei2 screenPos);
+	
 
 private:
 	Tile& TileAt(const Vei2& gridPos);
 	const Tile& TileAt(const Vei2& gridPos) const;
 	Vei2 ScreenToGrid(const Vei2& screenPos);
+	int CountNeighborBombs(const Vei2& gridPos);
 
 private:
 
